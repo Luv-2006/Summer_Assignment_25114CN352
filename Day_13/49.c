657e8d92992b31acef48ffc7dfa5c16ac5c72b06
Luv Bhati <luvbhati01@gmail.com>
2026-06-13 01:08:55 +0530

    Update 49.c

--- Day_13/49.c
+++ Day_13/49.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int arr[100], n, i;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Array Elements:\n");
+
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
