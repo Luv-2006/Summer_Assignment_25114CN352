f925dc028edcdb1367b70bad65fee43aaba8de7b
Luv Bhati <luvbhati01@gmail.com>
2026-06-13 01:07:55 +0530

    Update 46.c

--- Day_12/46.c
+++ Day_12/46.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int armstrong(int n) {
+    int temp = n, sum = 0, digit;
+
+    while(temp != 0) {
+        digit = temp % 10;
+        sum += digit * digit * digit;
+        temp /= 10;
+    }
+
+    return sum == n;
+}
+
+int main() {
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if(armstrong(n))
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
