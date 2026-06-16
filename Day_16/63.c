302ed1ee23166f249ba220bbf3168c4353851612
Luv Bhati <luvbhati01@gmail.com>
2026-06-16 19:38:58 +0530

    Create 63.c

---
+++ Day_16/63.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, sum;
+    int found = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == sum) {
+                printf("Pair Found: %d + %d = %d\n",
+                       arr[i], arr[j], sum);
+                found = 1;
+            }
+        }
+    }
+
+    if(!found)
+        printf("No pair found.");
+
+    return 0;
+}
